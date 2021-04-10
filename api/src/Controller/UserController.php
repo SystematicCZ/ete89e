e913c71029012cbc2b3e56609434717bae2af3f5
pritasil <marek.pritasil@startupjobs.cz>
2021-04-10 11:48:14 +0200

    progress

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -23,4 +23,17 @@ class UserController extends AbstractController
 
         return $this->json($view->createList($users));
    }
+
+    /**
+     * @Route("/users/{id}", name="user", methods={"GET"})
+     *
+     * @param UserRepository $repository
+     * @param UserView $view
+     * @param int $id
+     * @return Response
+     */
+   public function detail(UserRepository $repository, UserView $view, int $id): Response
+   {
+       return $this->json($view->create($repository->findOneBy(['id' => $id])));
+   }
 }
